2905bfa6dadc8ba87ab09ce88ca1ddb90649566f
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-05 14:52:55 -0400

    writing the readme file function and function to initialize

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { error } = require("console");
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -38,14 +39,33 @@ const questions = [
         name: "tests",
         message: "Provide test instructions:"
     }
-]; 
+];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} created successfully!`);
+        }
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .Prompt(questions)
+    .then((answers) =>{
+        const readmeContent = generateMarkdown(answers);
+
+        writeToFile("README.md", readmeContent);
+    })
+    .catch((error) =>{
+        console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
