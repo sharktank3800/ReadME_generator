8ce0b9d3d8a37aeb28a705d4bb4682a4c4e2a2a0
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-05 15:29:30 -0400

    forgot to import generateMarkdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./generateMarkdown.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { error } = require("console");
 // TODO: Create an array of questions for user input
 const questions = [
     {
