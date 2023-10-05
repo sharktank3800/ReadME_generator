a70288ce5941b518a535b443addd337f050b32a0
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-05 14:37:28 -0400

    included packages needed, added questions for the app

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project:"
+    },
+
+    {
+        type: "input",
+        name: "Installation",
+        message: "Provide installation instructions of your project:"
+    },
+
+    {
+        type: "input",
+        name: "Usage",
+        message: "Provide Usage information:"
+    },
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Provide contribution guidelines:"
+    },
+
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide test instructions:"
+    }
+]; 
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
